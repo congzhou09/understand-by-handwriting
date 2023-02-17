fd63ba8274769356fbaf7909b14a939f4ee15267
congzhou09 <yuancongzhou@yeah.net>
2023-02-17 21:56:12 +0800

    chore:curry refine

--- src/tool/currying.js
+++ src/tool/currying.js
@@ -1,25 +1,24 @@
 function currying() {
   if (arguments.length < 1) {
-    throw new Error(`At least the function is needed as the first param.`);
+    throw new Error(`Function name param is needed.`);
   }
-  const func = arguments[0];
-  const funcParamLen = func?.length;
-  let params = Array.prototype.slice.call(arguments, 1) ?? [];
-  if (params.length >= funcParamLen) {
-    return func.apply(params);
+  const fun = arguments[0];
+  let params = Array.prototype.slice.call(arguments, 1);
+  if (params.length >= fun.length) {
+    return fun(...params);
   }
 
-  function curryCore() {
+  function getNextCurriedFun() {
     return function () {
       params = Array.prototype.concat.apply(params, arguments);
-      if (params.length >= funcParamLen) {
-        return func(...params);
+      if (params.length >= fun.length) {
+        return fun(...params);
       } else {
-        return curryCore();
+        return getNextCurriedFun();
       }
     };
   }
-  return curryCore();
+  return getNextCurriedFun();
 }
 
 export default currying;
