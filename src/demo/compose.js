6ab3d1e90ad4344a81c7a014358efaa827d63694
congzhou09 <yuancongzhou@yeah.net>
2023-03-09 18:55:07 +0800

    feat:compose revise

--- src/demo/compose.js
+++ src/demo/compose.js
@@ -11,27 +11,33 @@ function wait(sec = 0) {
   });
 }
 
+function pushAndConsole(...params) {
+  dataArr.push(...params);
+  console.log(...params);
+}
+
 const delaySec = 1;
 
 middlewareArr.push(async (context, next) => {
-  dataArr.push(1);
+  pushAndConsole(1);
   await wait(delaySec);
   await next();
   await wait(delaySec);
-  dataArr.push(2);
+  pushAndConsole(2);
 });
 
 middlewareArr.push(async (context, next) => {
-  dataArr.push(3);
+  pushAndConsole(3);
   await wait(delaySec);
   await next();
+  // await next();
   await wait(delaySec);
-  dataArr.push(4);
+  pushAndConsole(4);
 });
 
 (async () => {
   await MyCompose(middlewareArr)({}, () => {
-    dataArr.push(0);
+    pushAndConsole(0);
   });
   console.log(dataArr);
 })();
--- src/tool/my-compose.js
+++ src/tool/my-compose.js
@@ -1,18 +1,29 @@
 function MyCompose(middlewareArr) {
   if (!Array.isArray(middlewareArr)) {
-    throw new Error(`input param need to be an array`);
+    throw new TypeError(`The input param must be an array`);
+  }
+  for (let fn of middlewareArr) {
+    if (typeof fn != "function") {
+      throw new TypeError(`Members of middleware must be functions`);
+    }
   }
 
   return (context, next) => {
+    let index = -1;
     return dispatch(0);
     function dispatch(i) {
+      if (i <= index) {
+        return Promise.reject(new Error(`next() called multiple times`));
+      }
+      index = i;
       let fn = middlewareArr[i];
-      if (i === middlewareArr.length) {
+      if (i == middlewareArr.length) {
         fn = next;
       }
       if (!fn) {
         return Promise.resolve();
       }
+
       try {
         return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
       } catch (err) {
